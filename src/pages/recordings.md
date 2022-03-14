2467c48d084b72d61ce859c4ff426a14099f7fce
Ollie <oliver@bassett.net.nz>
2022-03-14 09:44:11 +0900

    week2 - added automatic page generation. Added mypy dev dependencies and updated pytest-version in preperation for following week3.

---
+++ src/pages/recordings.md
@@ -0,0 +1,6 @@
+## Our Recordings
+
+- Session 1
+- Session 2
+- Session 3
+- Session 4
--- src/tps_blog/main.py
+++ src/tps_blog/main.py
@@ -1,18 +1,36 @@
-from pathlib import Path
+from aiopath import AsyncPath
+from aiofile import async_open
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from markdown import markdown
 
 # TODO: Convert the paths to async paths using aiofiles and aiopath
 
-static_path = Path(__file__).parent.parent / "static"
-templates_path = Path(__file__).parent.parent / "templates"
+static_path = AsyncPath(__file__).parent.parent / "static"
+templates_path = AsyncPath(__file__).parent.parent / "templates"
+
 
 # TODO: Add pages directory to contain markdown content for pages - so I don't have to write html
 # The pages directory should sit under src already and contains already has a few markdown files.
+pages_path = AsyncPath(__file__).parent.parent / "pages"
+
+
+async def get_navbar_items(pages_path: AsyncPath) -> list[str]:
+    navbar = []
+    async for page in pages_path.glob("*.md"):
+        navbar.append(page.stem)
+    return navbar
+
+
+async def render_markdown_to_html(markdown_page_path: AsyncPath) -> str:
+    async with async_open(markdown_page_path) as f:
+        content = await f.read()
+    return markdown(content)
+
 
 app = FastAPI()
 
@@ -23,10 +41,32 @@ templates = Jinja2Templates(directory=templates_path)
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    content = "Hello World"
+    page_title = "Home"
+    content = "Hello Tokyo Python Society"
+    navbar_items = await get_navbar_items(pages_path)
     return templates.TemplateResponse(
-        "base.html.j2", {"request": request, "content": content}
+        "pages.html.j2",
+        {
+            "request": request,
+            "content": content,
+            "navbar_items": navbar_items,
+            "page_title": page_title,
+        },
     )
 
 
 # TODO: Add a /pages/<page_name> route to serve pages from the pages directory
+@app.get("/pages/{page_name}", response_class=HTMLResponse)
+async def get_pages(request: Request, page_name: str):
+    markdown_page = pages_path / f"{page_name}.md"
+    content = await render_markdown_to_html(markdown_page)
+    navbar_items = await get_navbar_items(pages_path)
+    return templates.TemplateResponse(
+        "pages.html.j2",
+        {
+            "request": request,
+            "content": content,
+            "navbar_items": navbar_items,
+            "page_title": page_name,
+        },
+    )
